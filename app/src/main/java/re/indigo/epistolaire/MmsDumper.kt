ec702c9afdc46cb33cc2f5a8b7c82b131606d595
hydrargyrum <dev@indigo.re>
2019-11-24 15:16:15 +0100

    organize imports

--- app/src/main/java/re/indigo/epistolaire/MmsDumper.kt
+++ app/src/main/java/re/indigo/epistolaire/MmsDumper.kt
@@ -10,10 +10,7 @@ import android.net.Uri
 import android.util.Base64
 import org.json.JSONArray
 import org.json.JSONObject
-import java.io.BufferedReader
 import java.io.IOException
-import java.io.InputStream
-import java.io.InputStreamReader
 import java.nio.charset.Charset
 
 
