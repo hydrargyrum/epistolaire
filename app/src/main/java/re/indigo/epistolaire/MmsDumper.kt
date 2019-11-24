f79d0256adea1f5e37f60efcb8335328504de73f
hydrargyrum <dev@indigo.re>
2019-11-24 15:14:59 +0100

    add license info in sources

--- app/src/main/java/re/indigo/epistolaire/MmsDumper.kt
+++ app/src/main/java/re/indigo/epistolaire/MmsDumper.kt
@@ -1,3 +1,8 @@
+/*
+ This is free and unencumbered software released into the public domain.
+ See LICENSE file for details.
+ */
+
 package re.indigo.epistolaire
 
 import android.content.ContentResolver
--- viewer/converter.py
+++ viewer/converter.py
@@ -1,3 +1,5 @@
+# This is free and unencumbered software released into the public domain.
+# See LICENSE file for details.
 
 import locale
 import datetime
