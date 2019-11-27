45f34dcd79eee80eb61c8d6ed958999770aaad59
hydrargyrum <dev@indigo.re>
2019-11-28 00:13:41 +0100

    ui: dump in background to avoid freezing UI and display animation
    Use an AsyncTask for this. Also show a progressbar (though
    indeterminate).
    
    Try to show errors if some happen.

--- app/src/main/java/re/indigo/epistolaire/MainActivity.kt
+++ app/src/main/java/re/indigo/epistolaire/MainActivity.kt
@@ -7,16 +7,17 @@ package re.indigo.epistolaire
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
-
+import kotlinx.android.synthetic.main.content_main.*
+import org.json.JSONObject
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
@@ -33,39 +34,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        /*
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+         */
 
         val wtf = 2
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_SMS), wtf)
         }
 
-        saveMessages()
+        addLine("Starting backup")
+        DumpTask().execute()
     }
 
-    fun saveMessages() {
-        //val dumper = SmsDumper(contentResolver)
-        val dumper = MmsDumper(contentResolver)
-        val jobj = dumper.getJson()
-
-        val myExternalFile = File(getExternalFilesDir("."), "backup.json")
-        Log.e("TAG", "plop ${myExternalFile}")
-        try {
-            val fileOutPutStream = FileOutputStream(myExternalFile)
-
-            try {
-                fileOutPutStream.write(jobj.toString().toByteArray())
-            } finally {
-                fileOutPutStream.close()
-            }
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-
-        Toast.makeText(applicationContext,"Backup done to ${myExternalFile}", Toast.LENGTH_LONG).show()
+    fun addLine(line: String) {
+        myText.text = myText.text.toString() + "\n" + line
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -81,6 +67,56 @@ class MainActivity : AppCompatActivity() {
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    inner class DumpTask : AsyncTask<Void, Int, JSONObject?>() {
+        val dumper = MmsDumper(contentResolver)
+        var exc : String? = null
+
+        override fun doInBackground(vararg params: Void?): JSONObject? {
+            try {
+                return dumper.getJson()
+            } catch (e: Exception) {
+                exc = e.toString()
+                return null
+            }
+        }
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+
+            progressBar.visibility = View.VISIBLE
+        }
+
+        override fun onPostExecute(jobj: JSONObject?) {
+            super.onPostExecute(jobj)
+
+            if (exc != null) {
+                progressBar.visibility = View.GONE
+                addLine(exc!!)
+                return
+            }
+
+            // TODO: ask where to save? ACTION_OPEN_DOCUMENT_TREE
+
+            val myExternalFile = File(getExternalFilesDir("."), "backup.json")
+            Log.e("TAG", "plop ${myExternalFile}")
+            try {
+                val fileOutPutStream = FileOutputStream(myExternalFile)
+
+                try {
+                    fileOutPutStream.write(jobj.toString().toByteArray())
+                } finally {
+                    fileOutPutStream.close()
+                }
+            } catch (e: IOException) {
+                addLine(e.toString())
+            }
+
+            progressBar.visibility = View.GONE
+
+            addLine("Backup done to $myExternalFile")
         }
     }
 }
