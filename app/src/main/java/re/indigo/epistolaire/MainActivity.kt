72d74e6d375530eb53cadf16744bf997eaacce27
hydrargyrum <dev@indigo.re>
2020-02-08 21:38:30 +0100

    ui/menu: remove "settings" entry because there aren't any yet

--- app/src/main/java/re/indigo/epistolaire/MainActivity.kt
+++ app/src/main/java/re/indigo/epistolaire/MainActivity.kt
@@ -66,7 +66,6 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
     }
